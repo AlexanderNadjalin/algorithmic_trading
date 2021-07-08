28b3298a816bc57a8eaf48216c0b539597d068f8
Alex <alexander@nadjalin.com>
2021-07-08 14:23:34 +0200

    Refactored position.py. Transaction history handled better, allows short selling.

--- dev.py
+++ dev.py
@@ -12,14 +12,15 @@ def dev():
     # print(market.select(['XACTOMXS30.ST'], '2021-05-01', '2021-05-06'))
 
     t1 = Transaction(name='AMZ', direction='B', quantity=25.0, price=150.0, commission_scheme='', date='2021-07-07')
-    t2 = Transaction(name='AMZ', direction='B', quantity=15.0, price=250.0, commission_scheme='', date='2021-07-07')
-    t3 = Transaction(name='AMZ', direction='B', quantity=10.0, price=300.0, commission_scheme='', date='2021-07-07')
-    t4 = Transaction(name='AMZ', direction='B', quantity=10.0, price=400.0, commission_scheme='', date='2021-07-07')
+    t2 = Transaction(name='AMZ', direction='S', quantity=15.0, price=151.0, commission_scheme='', date='2021-07-08')
+    t3 = Transaction(name='AMZ', direction='B', quantity=10.0, price=300.0, commission_scheme='avanza_medium', date='2021-07-09')
+    t4 = Transaction(name='AMZ', direction='B', quantity=10.0, price=400.0, commission_scheme='avanza_medium', date='2021-07-10')
 
-    p = Position(t1)
+    p = Position()
+    p.transact_position(t1)
     p.transact_position(t2)
-    p.transact_position(t3)
-    p.transact_position(t4)
+    # p.transact_position(t3)
+    # p.transact_position(t4)
     pass
 
 
