d4809fc931a012d25eb71eafce1c866a6d3f5c63
Alex <alexander@nadjalin.com>
2021-07-15 16:31:33 +0200

    portfolio.py first version completed. Added commission_scheme.py with avanza.se schemes.

--- dev.py
+++ dev.py
@@ -8,26 +8,48 @@ market_file_ane = 'test_data_ETF.csv'
 
 
 def dev():
-    # market = Market(market_file_name=market_file_ane,
-    #                     fill_missing_method='forward')
-    # print(market.select(['XACTOMXS30.ST'], '2021-05-01', '2021-05-06'))
+    market = Market(market_file_name=market_file_ane,
+                    fill_missing_method='forward')
 
-    t1 = Transaction(name='AMZ', direction='B', quantity=25.0, price=150.0, commission_scheme='', date='2021-07-07')
-    t2 = Transaction(name='AMZ', direction='B', quantity=25.0, price=151.0, commission_scheme='', date='2021-07-08')
-    t3 = Transaction(name='AMZ', direction='B', quantity=10.0, price=300.0, commission_scheme='avanza_medium', date='2021-07-09')
-    t4 = Transaction(name='AMZ', direction='B', quantity=10.0, price=400.0, commission_scheme='avanza_medium', date='2021-07-10')
+    t1 = Transaction(name='XACTOMXS30.ST',
+                     direction='B',
+                     quantity=10.0,
+                     price=286.85,
+                     commission_scheme='avanza_small',
+                     date='2021-05-03')
+    t2 = Transaction(name='XACTOMXS30.ST',
+                     direction='B',
+                     quantity=10.0,
+                     price=281.85,
+                     commission_scheme='avanza_small',
+                     date='2021-05-04')
+    t3 = Transaction(name='XACTOMXS30.ST',
+                     direction='S',
+                     quantity=20.0,
+                     price=291.15,
+                     commission_scheme='avanza_small',
+                     date='2021-05-05')
 
-    p = Position()
-    p.transact_position(t1)
-    p.transact_position(t2)
-    p.transact_position(t3)
-    p.transact_position(t4)
+    t4 = Transaction(name='SXRT.TG',
+                     direction='B',
+                     quantity=10.0,
+                     price=132.4,
+                     commission_scheme='avanza_medium',
+                     date='2021-05-03')
 
-    pf = Portfolio(inception_date='2021-05-06')
-    pf.add_position(p)
+    pf = Portfolio(inception_date='2021-05-03')
+    pf.transact_security(t1)
+    pf.update_all_market_values(date=t1.date,
+                                market_data=market)
 
-    pf.market_value()
-    print(str(pf.market_value))
+    pf.transact_security(t2)
+    pf.update_all_market_values(date=t2.date,
+                                market_data=market)
+
+    pf.transact_security(t3)
+    pf.update_all_market_values(date=t3.date,
+                                market_data=market)
+    print(pf.total_realized_pnl)
     pass
 
 
--- holdings/commission_scheme.py
+++ holdings/commission_scheme.py
@@ -7,15 +7,30 @@ class CommissionScheme:
     def calculate_commission(self,
                              quantity: float,
                              price: float) -> float:
-        min_com = 0.0
-        trans_com = 0.0
-
-        if self.name == 'avanza_medium':
-            min_com = 69
-            trans_com = quantity * price * 0.00069
-            if min_com <= trans_com:
+        # Avanza.se
+        if self.name == 'avanza_mini':
+            min_com = 1.0
+            trans_com = quantity * price * 0.0025
+            if quantity * price < 400.0:
                 return min_com
             else:
                 return trans_com
+        elif self.name == 'avanza_small':
+            min_com = 39.0
+            trans_com = quantity * price * 0.0015
+            if quantity * price < 26000.0:
+                return min_com
+            else:
+                return trans_com
+        elif self.name == 'avanza_medium':
+            min_com = 69.0
+            trans_com = quantity * price * 0.00069
+            if quantity * price < 100000.0:
+                return min_com
+            else:
+                return trans_com
+        elif self.name == 'avanza_fast':
+            return 99.0
+        # No commission
         elif self.name == '':
             return 0.0
